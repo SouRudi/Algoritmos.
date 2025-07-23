8327e8447f60b5c8b6b8c3686cf2c66db36ef24b
SouRudi <rudyeregabriel2008@gmail.com>
2025-07-23 10:51:40 -0300

    Update b.c

--- Atividades-em-C/atividade1/b.c
+++ Atividades-em-C/atividade1/b.c
@@ -13,18 +13,18 @@ int main()
      
      printf("----------------------------------------\n");
      
-     int soma = num1 + num2;
-     int subtracacao = num1 - num2;
-     float divisao = num1/num2;
-     float resto = num1 % num2;
-     int multiplicacao = num1* num2;
+     int sm = num1 + num2;
+     int sub = num1 - num2;
+     float div = num1/num2;
+     float res = num1 % num2;
+     int mult = num1* num2;
      
      
-     printf("A soma entre esses numeros e: %d\n", soma);
-     printf("A subtracacao entre esses numeros e: %d\n", subtracacao);
-     printf("A divisao entre esses numeros e: %.0f\n", divisao);
-     printf("A resto entre esses numeros e: %.2f\n", resto);
-     printf("A multiplicacao entre esses numeros e: %d\n", multiplicacao);
+     printf("A soma entre esses numeros e: %d\n", sm);
+     printf("A subtracacao entre esses numeros e: %d\n", subt);
+     printf("A divisao entre esses numeros e: %.0f\n", divi);
+     printf("A resto entre esses numeros e: %.2f\n", res);
+     printf("A multiplicacao entre esses numeros e: %d\n", mult);
      
      return 0;
 }
