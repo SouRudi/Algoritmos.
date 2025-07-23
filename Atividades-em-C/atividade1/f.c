1bf087009301d5f8671c5c7ed21927de6ee74286
SouRudi <rudyeregabriel2008@gmail.com>
2025-07-23 10:54:22 -0300

    Update f.c

--- Atividades-em-C/atividade1/f.c
+++ Atividades-em-C/atividade1/f.c
@@ -5,21 +5,16 @@ int main()
     float raio;
     float alt;
 
-
     printf("Qual a altura da embalagem: \n");
     scanf("%f", &alt);
-
     printf("Qual o raio da embalagem: \n");
     scanf("%f", &raio);
 
     float vol = 3.14159 * raio * alt;
-
-    printf("O volume da embalagem e : %.2f\n ", vol);
+    printf("O volume da embalagem eh: %.2f\n ", vol);
 
     float ml = vol*1000;
-
-
-    printf("O volume em litros e: %.1f (L)\n", ml);
+    printf("O volume em litros eh: %.1f (L)\n", ml);
 
     return 0;
 }
