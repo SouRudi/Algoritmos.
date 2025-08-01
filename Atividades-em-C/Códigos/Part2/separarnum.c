95958385c5e8c695f077b460a179a4580cc10e73
SouRudi <rudyeregabriel2008@gmail.com>
2025-08-01 11:00:35 -0300

    Create separarnum.c

---
+++ Atividades-em-C/Códigos/Part2/separarnum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+ 
+    printf("Digite um número de cinco dígitos: ");
+    scanf("%d", &num);
+
+    printf("Dígitos separados: ");
+    printf("%d ", num / 10000);   
+    num %= 10000;
+    printf("%d ", num / 1000);    
+    num %= 1000;
+    printf("%d ", num / 100);     
+    num %= 100;
+    printf("%d ", num / 10);     
+    num %= 10;
+    printf("%d\n", num);        
+
+    return 0;
+}
