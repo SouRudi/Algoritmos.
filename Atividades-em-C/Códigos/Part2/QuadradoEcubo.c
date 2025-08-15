edfb28d29c65e913eab84f98b43292ca183d441f
SouRudi <rudyeregabriel2008@gmail.com>
2025-08-15 09:11:27 -0300

    Update QuadradoEcubo.c

--- Atividades-em-C/Códigos/Part2/QuadradoEcubo.c
+++ Atividades-em-C/Códigos/Part2/QuadradoEcubo.c
@@ -3,6 +3,6 @@
 
 int main(){
     for (int n = 0; n <= 10; n++){
-        printf("Numero\tQuadrado\tCubo\n%d\t\t\t%d\t\t%d\n", n, n * n, n * n * n);
+        printf("Numero\tQuadrado\tCubo\n%d\t\%d\t\t%d\n", n, n * n, n * n * n);
     }
 }
