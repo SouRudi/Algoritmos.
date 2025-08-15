21d6eefe89641f6a5b66d76e1e816cd2b3ce438b
SouRudi <rudyeregabriel2008@gmail.com>
2025-08-15 09:33:07 -0300

    Update divisão.c

--- Atividades-em-C/Códigos/Part2/divisão.c
+++ Atividades-em-C/Códigos/Part2/divisão.c
@@ -3,18 +3,29 @@
 int main() {
 
     int num, dois, tres;
-    
+
     printf("\t\tDigite um número: ");
     scanf("%d", &num);
-    
+
     dois = num%2;
     tres = num%3;
-    
+
+    if(dois == 0){
+    printf("\nÉ divisível por 2");
+    }else{
+    printf("Não é divisível por 2");
+    }
+    if(tres == 0){
+    printf(" e divisível por 3");
+    }else{
+    printf(" e não é divisível por 3");
+    }
+
     if(dois == 0 && tres == 0){
     printf("\n%d é divisível por 2 e 3 simultaneamente", num);
     }else{
     printf("\n%d NÃO é divisível por 2 e 3 simultaneamente", num);
     }
-    
+
     return 0;
 }
